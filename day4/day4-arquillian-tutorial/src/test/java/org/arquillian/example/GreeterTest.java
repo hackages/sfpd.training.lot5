0abfa7894e3dbada64634004a886e3600a00ed3a
Tata DA SILVEIRA <dasilveira.tata@gmail.com>
2020-11-20 08:31:27 +0100

    added day5 sources

---
+++ day4/day4-arquillian-tutorial/src/test/java/org/arquillian/example/GreeterTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.arquillian.example;
+
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Set;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author <a href="http://community.jboss.org/people/dan.j.allen">Dan Allen</a>
+ */
+@RunWith(Arquillian.class)
+public class GreeterTest {
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        JavaArchive jar = ShrinkWrap.create(JavaArchive.class)
+            .addClasses(Greeter.class, PhraseBuilder.class)
+            .addAsManifestResource("beans.xml", "beans.xml");
+        return jar;
+    }
+
+    @Inject
+    Greeter greeter;
+
+    @Inject
+    BeanManager beanManager;
+
+    @Test
+    public void should_create_greeting() {
+        Set<Bean<?>> disabledBeans = beanManager.getBeans(Greeter.class);
+        assertFalse("Instances of bean expected.", disabledBeans.isEmpty());
+        assertTrue(StringUtils.equals("Hello, Test!", greeter.createGreeting("Test")));
+    }
+}
